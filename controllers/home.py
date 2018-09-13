3db1dadbed5a7d603bd78f3601509c2f497d09f6
Mazzen <mnsaab@umich.edu>
2018-09-13 13:26:07 -0400

    fixed duplicate files

--- controllers/home.py
+++ controllers/home.py
@@ -5,5 +5,5 @@ import os
 home = Blueprint('home', __name__, template_folder='templates')
 
 @home.route('/', methods=['GET', 'POST'])
-def mainApi():
+def homeApi():
 	return render_template("base.html")
--- home.py
+++
@@ -1,9 +0,0 @@
-from flask import *
-from extensions import db
-import os
-
-home = Blueprint('home', __name__, template_folder='templates')
-
-@home.route('/', methods=['GET', 'POST'])
-def homeApi():
-	return render_template("base.html")
