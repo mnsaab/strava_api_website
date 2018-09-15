bb457bcea484c696ede20e644d40582de8b916b9
Mazzen <mnsaab@umich.edu>
2018-09-15 01:34:56 -0400

    rearranged files for better organization and improved piping

--- api/api.py
+++ api/api.py
@@ -7,52 +7,43 @@ import uuid
 
 app = Blueprint('api', __name__, template_folder='templates')
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/api/home', methods=['GET', 'POST'])
 def homeApi():
-	if 'username' not in session:
-		return redirect(url_for("api.loginApi"))
+	return "displaying data from database is done here. Calling strava api is done through javascript in html file"		
 
-	else:
-		if request.method == "GET":
-			return render_template("base.html")
-		else:
-			return "displaying data from database is done here. Calling strava api is done through javascript in html file"		
-
-			# return render_template("base.html")		
+		# return render_template("base.html")		
 
 
 
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/api/login', methods=['POST'])
 def loginApi():
-	if request.method == "GET":
-		return render_template("login.html")
-	else:
-		vals = request.get_json()
-		# print(vals['username'])
 
-		username = vals['username']
-		password = vals['password']
+	vals = request.get_json()
+	# print(vals['username'])
 
-		# For when the password is encrypted in the database, I need to do some encrypting and decrypting to match the passwords 
+	username = vals['username']
+	password = vals['password']
 
-		cur = db.cursor()
-		
-		query = "SELECT * FROM accounts WHERE username=\"" + username + "\""
-		cur.execute(query)
-		dbData = cur.fetchone()
+	# For when the password is encrypted in the database, I need to do some encrypting and decrypting to match the passwords 
 
-		if (dbData == None):
-			return jsonify("This username cannot be found, if you don't have an account, please make one. Otherwise, please try again"), 403
+	cur = db.cursor()
+	
+	query = "SELECT * FROM accounts WHERE username=\"" + username + "\""
+	cur.execute(query)
+	dbData = cur.fetchone()
 
-		if (dbData['password'] != password):
-			return jsonify("This password does not match with the given username, please try again"), 403
+	if (dbData == None):
+		return jsonify("This username cannot be found, if you don't have an account, please make one. Otherwise, please try again"), 403
 
-		session['username'] = username
-		# session['firstname'] = dbData['firstname']
-		# session['lastname'] = dbData['lastname']
+	if (dbData['password'] != password):
+		return jsonify("This password does not match with the given username, please try again"), 403
+
+	session['username'] = username
+	# session['firstname'] = dbData['firstname']
+	# session['lastname'] = dbData['lastname']
 
 
-		return jsonify("Successfully logged in as " + session['username']), 203
+	return jsonify("Successfully logged in as " + session['username']), 203
 
 
 
@@ -60,42 +51,40 @@ def loginApi():
 def logoutApi():
 	session.pop('username', None)
 
-	return redirect(url_for("api.loginApi"))
+	return redirect(url_for("home.login_route"))
 
 
-@app.route('/create-account', methods=['POST', 'GET'])
+@app.route('/api/create-account', methods=['POST'])
 def createApi():
-	if request.method == "GET":
-		return render_template("createAccount.html")
-	else:
-		vals = request.get_json()
-		# print(vals['username'])
 
-		username = vals['username']
-		firstname = vals['firstname']
-		lastname = vals['lastname']
-		password = vals['password']
+	vals = request.get_json()
+	# print(vals['username'])
 
-		cur = db.cursor()
+	username = vals['username']
+	firstname = vals['firstname']
+	lastname = vals['lastname']
+	password = vals['password']
 
-		# I need to add a hashing algorithm to secure the passwords in the databases 
+	cur = db.cursor()
 
-		query = "SELECT username FROM accounts WHERE username=\"" + username + "\""
-		cur.execute(query)
-		dbData = cur.fetchone()
+	# I need to add a hashing algorithm to secure the passwords in the databases 
 
-		if (dbData != None):
-			return jsonify("This username already in use, please try something else"), 403
+	query = "SELECT username FROM accounts WHERE username=\"" + username + "\""
+	cur.execute(query)
+	dbData = cur.fetchone()
 
-		query = "SELECT password FROM accounts WHERE password=\"" + password + "\""
-		cur.execute(query)
-		dbData = cur.fetchone()
+	if (dbData != None):
+		return jsonify("This username already in use, please try something else"), 403
 
-		if (dbData != None):
-			return jsonify("This password is already in use, please try something else"), 403
+	query = "SELECT password FROM accounts WHERE password=\"" + password + "\""
+	cur.execute(query)
+	dbData = cur.fetchone()
 
-		query = "INSERT INTO accounts (username, password, firstname, lastname) VALUES (\"" + username + "\", \"" + password + "\", \"" + firstname + "\", \"" + lastname + "\")"   
-		cur.execute(query)
+	if (dbData != None):
+		return jsonify("This password is already in use, please try something else"), 403
 
-		confirm = "created account for " + vals['firstname'] + " " + vals['lastname']
-		return jsonify(confirm), 203
+	query = "INSERT INTO accounts (username, password, firstname, lastname) VALUES (\"" + username + "\", \"" + password + "\", \"" + firstname + "\", \"" + lastname + "\")"   
+	cur.execute(query)
+
+	confirm = "created account for " + vals['firstname'] + " " + vals['lastname']
+	return jsonify(confirm), 203
